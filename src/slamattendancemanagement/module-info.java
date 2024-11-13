5dc4347c6c5af00205b370b27e36f30df94844fc
000Shreeharish000 <shreeharishv@gmail.com>
2024-11-13 23:23:03 +0530

    Add src/slamattendancemanagement folder

---
+++ src/slamattendancemanagement/module-info.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+package slamattendancemanagement;
+
+
+module slamattendancemanagement {
+    exports Attendance;
+    requires java.sql;
+    requires java.desktop;
+}
